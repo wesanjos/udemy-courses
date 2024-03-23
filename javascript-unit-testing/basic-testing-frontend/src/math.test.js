f826a1b64557c0837d5a924b399cc99afd93dc06
Wesley Anjos <wanjos.andrade@gmail.com>
2024-03-23 13:37:03 -0300

    test: verificação de números NaN e formatos em string

--- javascript-unit-testing/basic-testing-frontend/src/math.test.js
+++ javascript-unit-testing/basic-testing-frontend/src/math.test.js
@@ -2,7 +2,35 @@ import { it, expect } from "vitest";
 import { add } from "./math";
 
 it("should summarize all number values in an array", () => {
-  const result = add([1, 2, 3]);
+  //Pattern AAA - Arrange, Act, Assert
+  const numbers = [1, 2, 3];
+  const expectedResult = numbers.reduce(
+    (prevValue, curValue) => prevValue + curValue,
+    0
+  );
 
-  expect(result).toBe(6);
+  const result = add(numbers);
+
+  expect(result).toBe(expectedResult);
+});
+
+it("should yield NaN if a least one invalid number is provided", () => {
+  const inputs = ["Invalid", 1];
+
+  const result = add(inputs);
+
+  expect(result).toBeNaN();
+});
+
+it("should yield a correct sum if an array of numeric string values is provided", () => {
+  const numbers = ["1", "2"];
+
+  const result = add(numbers);
+
+  const expectedResult = numbers.reduce(
+    (prevValue, curValue) => +prevValue + +curValue,
+    0
+  );
+
+  expect(result).toBe(expectedResult);
 });
